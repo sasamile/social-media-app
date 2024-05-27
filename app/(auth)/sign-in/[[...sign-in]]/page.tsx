1fedec9a1017b5f01edc5575b085b54394fe8a72
Santiago Suescun <116961553+sasamile@users.noreply.github.com>
2024-05-27 12:13:10 -0500

    Sign-up updated

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
                 "bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
               headerTitle: "text-lg font-bold mb-4",
               headerSubtitle: "text-sm text-gray-500",
-              footer: "hidden",
+             
             },
             variables: {
               colorPrimary: "#0070f3",
--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 // Define un array de rutas públicas
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
+  "/sign-up(.*)",
   "/api/webhooks(.*)",
 ]);
 
